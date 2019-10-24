e96b16dcd6e5ba026125900e9cf157146b273661
ivan <trainees@netls.com.ua>
2019-10-24 17:00:23 +0300

    added Content-Security-Policy

--- src/components/EditNews/index.js
+++ src/components/EditNews/index.js
@@ -7,17 +7,6 @@ const NO_PHOTO_IMG = "https://zenit.org/wp-content/uploads/2018/05/no-image-icon
 
 class EditNews extends Component {
 
-  titleStyle = {
-    color: '#000'
-  }
-
-  addDefaultSrc = (ev) => {
-    if(!this.props.img){
-      return ev.target.src = NO_PHOTO_IMG;
-    }else{
-      return ev.target.src = this.props.img;
-    }
-  }
 
   render() {
     let {
@@ -33,22 +22,9 @@ class EditNews extends Component {
     // const { isEdited } = this.state;
 
     let content = (
-      <tr className="odd gradeX" onClick={this.props.onToggleModal} style={{cursor: "pointer"}}>
-        <td>{id}</td>
-        <td>
-          <Link style={this.titleStyle} to={{ pathname: "/onenews", state: { id: id } }}>{title}</Link>
-        </td>
-        <td>
-          <img
-            src={!img ? this.src = NO_PHOTO_IMG : this.src = img}
-            alt="some"
-            height="42"
-            width="42"
-          />
-        </td>
-        <td>{publish_time}</td>
-        <td style={{ minWidth: "35px" }}>{description}</td>
-      </tr>
+      <>
+        
+      </>
     );
 
     return content;
